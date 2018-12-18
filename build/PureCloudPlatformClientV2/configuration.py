b74720a611f143d93590bdc8b27e4af27971e3e6
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2018-12-18 14:24:14 +0000

    47.1.2

--- build/PureCloudPlatformClientV2/configuration.py
+++ build/PureCloudPlatformClientV2/configuration.py
@@ -245,5 +245,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v2\n"\
-               "SDK Package Version: 47.1.1".\
+               "SDK Package Version: 47.1.2".\
                format(env=sys.platform, pyversion=sys.version)
