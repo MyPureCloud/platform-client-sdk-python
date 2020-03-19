81a4ef94b6cbb6be2d0f8890c154b5b2e1805b2a
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2020-03-19 19:10:44 +0000

    79.0.1

--- build/PureCloudPlatformClientV2/purecloud_region_hosts.py
+++ build/PureCloudPlatformClientV2/purecloud_region_hosts.py
@@ -7,10 +7,10 @@ class PureCloudRegionHosts(Enum):
     ap_southeast_2 = "https://api.mypurecloud.com.au"
     ap_northeast_1 = "https://api.mypurecloud.jp"
     eu_central_1 = "https://api.mypurecloud.de"
-    us_west_2 = "https://usw2.pure.cloud"
-    ca_central_1 = "https://cac1.pure.cloud"
-    ap_northeast_2 = "https://apne2.pure.cloud"
-    eu_west_2 = "https://euw2.pure.cloud"
+    us_west_2 = "https://api.usw2.pure.cloud"
+    ca_central_1 = "https://api.cac1.pure.cloud"
+    ap_northeast_2 = "https://api.apne2.pure.cloud"
+    eu_west_2 = "https://api.euw2.pure.cloud"
 
     def __init__(self, apihost):
         self.apihost = apihost
