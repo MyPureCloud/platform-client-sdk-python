c33ca6a2c26e52142a090be215c27c095ad0db1b
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2022-12-20 17:43:19 +0000

    160.0.1

--- build/setup.py
+++ build/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "PureCloudPlatformClientV2"
-VERSION = "160.0.0"
+VERSION = "160.0.1"
 
 # To install the library, run the following
 #
@@ -17,7 +17,7 @@ REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "wat
 
 setup(
     name="PureCloudPlatformClientV2",
-    version="160.0.0",
+    version="160.0.1",
     description="PureCloud Platform API SDK",
     author="Genesys Developer Evangelists",
     author_email="DeveloperEvangelists@Genesys.com",
