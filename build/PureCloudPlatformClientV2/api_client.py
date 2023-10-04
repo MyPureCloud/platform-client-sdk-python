95570f56f5fe5389366c2d47b0886f2dca34f5e6
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2023-10-04 13:10:31 +0000

    187.2.0

--- build/PureCloudPlatformClientV2/api_client.py
+++ build/PureCloudPlatformClientV2/api_client.py
@@ -295,7 +295,7 @@ class ApiClient(object):
             header_params['Cookie'] = self.cookie
         if header_params:
             header_params = self.sanitize_for_serialization(header_params)
-        header_params['purecloud-sdk'] = '187.1.0'
+        header_params['purecloud-sdk'] = '187.2.0'
 
         # path parameters
         if path_params:
