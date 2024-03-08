85fcc9e3538913bfddbad180c4ed97254b8cce7d
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2024-03-08 07:41:36 +0000

    196.1.0

--- build/PureCloudPlatformClientV2/models/contact_identifier.py
+++ build/PureCloudPlatformClientV2/models/contact_identifier.py
@@ -48,20 +48,50 @@ class ContactIdentifier(object):
                                   and the value is json key in definition.
         """
         self.swagger_types = {
+            'id': 'str',
             'type': 'str',
             'value': 'str',
-            'date_created': 'datetime'
+            'date_created': 'datetime',
+            'self_uri': 'str'
         }
 
         self.attribute_map = {
+            'id': 'id',
             'type': 'type',
             'value': 'value',
-            'date_created': 'dateCreated'
+            'date_created': 'dateCreated',
+            'self_uri': 'selfUri'
         }
 
+        self._id = None
         self._type = None
         self._value = None
         self._date_created = None
+        self._self_uri = None
+
+    @property
+    def id(self) -> str:
+        """
+        Gets the id of this ContactIdentifier.
+        The globally unique identifier for the object.
+
+        :return: The id of this ContactIdentifier.
+        :rtype: str
+        """
+        return self._id
+
+    @id.setter
+    def id(self, id: str) -> None:
+        """
+        Sets the id of this ContactIdentifier.
+        The globally unique identifier for the object.
+
+        :param id: The id of this ContactIdentifier.
+        :type: str
+        """
+        
+
+        self._id = id
 
     @property
     def type(self) -> str:
@@ -139,6 +169,30 @@ class ContactIdentifier(object):
         
 
         self._date_created = date_created
+
+    @property
+    def self_uri(self) -> str:
+        """
+        Gets the self_uri of this ContactIdentifier.
+        The URI for this object
+
+        :return: The self_uri of this ContactIdentifier.
+        :rtype: str
+        """
+        return self._self_uri
+
+    @self_uri.setter
+    def self_uri(self, self_uri: str) -> None:
+        """
+        Sets the self_uri of this ContactIdentifier.
+        The URI for this object
+
+        :param self_uri: The self_uri of this ContactIdentifier.
+        :type: str
+        """
+        
+
+        self._self_uri = self_uri
 
     def to_dict(self):
         """
